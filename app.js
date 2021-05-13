6afcbd4b2b0ad456cfc75082c86a4b7c8c2f386b
Ryan Park <ryan.developer99@gmail.com>
2021-05-13 18:13:48 +0900

    fix: obfuscate
    obfuscating the code

--- app.js
+++ app.js
@@ -1,42 +1,84 @@
-const input = document.querySelector(".input");
-const btn = document.querySelector(".btn");
-const img = document.querySelector(".img");
-const img2 = document.querySelector(".img2");
-
-const list = document.querySelector(".grid");
-
-btn.addEventListener("click", function () {
-  const value = input.value;
-  console.log(value);
-  const url = `https://api.unsplash.com/search/photos/?client_id=${yes}&query=${value}`;
-  console.log(url);
-
-  //   Make a request to API
-  fetch(url)
-    .then((response) => {
-      console.log(response);
-      if (response.ok) return response.json();
-      else alert("Wrong url");
+var _0xcdfb = [
+  "\x2E\x69\x6E\x70\x75\x74",
+  "\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72",
+  "\x2E\x62\x74\x6E",
+  "\x31\x61\x66\x4E\x6A\x43\x62\x36\x55\x37\x5A\x48\x45\x32\x39\x42\x32\x4A\x64\x33\x75\x37\x65\x35\x57\x74\x6E\x6A\x34\x41\x49\x52\x49\x78\x6A\x2D\x65\x76\x33\x36\x77\x69\x4D",
+  "\x2E\x69\x6D\x67",
+  "\x2E\x69\x6D\x67\x32",
+  "\x2E\x67\x72\x69\x64",
+  "\x63\x6C\x69\x63\x6B",
+  "\x76\x61\x6C\x75\x65",
+  "\x6C\x6F\x67",
+  "\x68\x74\x74\x70\x73\x3A\x2F\x2F\x61\x70\x69\x2E\x75\x6E\x73\x70\x6C\x61\x73\x68\x2E\x63\x6F\x6D\x2F\x73\x65\x61\x72\x63\x68\x2F\x70\x68\x6F\x74\x6F\x73\x2F\x3F\x63\x6C\x69\x65\x6E\x74\x5F\x69\x64\x3D",
+  "\x26\x71\x75\x65\x72\x79\x3D",
+  "",
+  "\x72\x65\x73\x75\x6C\x74\x73",
+  "\x68\x74\x6D\x6C",
+  "\x6C\x69\x6E\x6B\x73",
+  "\x74\x68\x65\x6E",
+  "\x6F\x6B",
+  "\x6A\x73\x6F\x6E",
+  "\x57\x72\x6F\x6E\x67\x20\x75\x72\x6C",
+  "\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72",
+  "\x64\x69\x76",
+  "\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74",
+  "\x63\x6C\x61\x73\x73\x4E\x61\x6D\x65",
+  "\x69\x6D\x67",
+  "\x62\x61\x63\x6B\x67\x72\x6F\x75\x6E\x64\x49\x6D\x61\x67\x65",
+  "\x73\x74\x79\x6C\x65",
+  "\x75\x72\x6C\x28",
+  "\x72\x65\x67\x75\x6C\x61\x72",
+  "\x75\x72\x6C\x73",
+  "\x29",
+  "\x64\x62\x6C\x63\x6C\x69\x63\x6B",
+  "\x64\x6F\x77\x6E\x6C\x6F\x61\x64",
+  "\x5F\x62\x6C\x61\x6E\x6B",
+  "\x6F\x70\x65\x6E",
+  "\x61\x70\x70\x65\x6E\x64\x43\x68\x69\x6C\x64",
+];
+const input = document[_0xcdfb[1]](_0xcdfb[0]);
+const btn = document[_0xcdfb[1]](_0xcdfb[2]);
+const yes = _0xcdfb[3];
+const img = document[_0xcdfb[1]](_0xcdfb[4]);
+const img2 = document[_0xcdfb[1]](_0xcdfb[5]);
+const list = document[_0xcdfb[1]](_0xcdfb[6]);
+btn[_0xcdfb[20]](_0xcdfb[7], function () {
+  const _0x8e87x7 = input[_0xcdfb[8]];
+  console[_0xcdfb[9]](_0x8e87x7);
+  const _0x8e87x8 = `${_0xcdfb[10]}${yes}${_0xcdfb[11]}${_0x8e87x7}${_0xcdfb[12]}`;
+  console[_0xcdfb[9]](_0x8e87x8);
+  fetch(_0x8e87x8)
+    [_0xcdfb[16]]((_0x8e87xa) => {
+      console[_0xcdfb[9]](_0x8e87xa);
+      if (_0x8e87xa[_0xcdfb[17]]) {
+        return _0x8e87xa[_0xcdfb[18]]();
+      } else {
+        alert(_0xcdfb[19]);
+      }
     })
-    .then((data) => {
-      console.log(data);
-      console.log(data.results);
-      console.log(data.results[0]);
-      console.log(data.results[5].links.html);
-      createImagesArray(data);
+    [_0xcdfb[16]]((_0x8e87x9) => {
+      console[_0xcdfb[9]](_0x8e87x9);
+      console[_0xcdfb[9]](_0x8e87x9[_0xcdfb[13]]);
+      console[_0xcdfb[9]](_0x8e87x9[_0xcdfb[13]][0]);
+      console[_0xcdfb[9]](_0x8e87x9[_0xcdfb[13]][5][_0xcdfb[15]][_0xcdfb[14]]);
+      createImagesArray(_0x8e87x9);
     });
 });
-
-function createImagesArray(data) {
-  const imageNodes = [];
-  for (let i = 6; i < 9; i++) {
-    imageNodes[i] = document.createElement("div");
-    imageNodes[i].className = "img";
-    imageNodes[i].style.backgroundImage =
-      "url(" + data.results[i].urls.regular + ")";
-    imageNodes[i].addEventListener("dblclick", function () {
-      window.open(data.results[i].links.download, "_blank");
+function createImagesArray(_0x8e87x9) {
+  const _0x8e87xc = [];
+  for (let _0x8e87xd = 6; _0x8e87xd < 9; _0x8e87xd++) {
+    _0x8e87xc[_0x8e87xd] = document[_0xcdfb[22]](_0xcdfb[21]);
+    _0x8e87xc[_0x8e87xd][_0xcdfb[23]] = _0xcdfb[24];
+    _0x8e87xc[_0x8e87xd][_0xcdfb[26]][_0xcdfb[25]] =
+      _0xcdfb[27] +
+      _0x8e87x9[_0xcdfb[13]][_0x8e87xd][_0xcdfb[29]][_0xcdfb[28]] +
+      _0xcdfb[30];
+    _0x8e87xc[_0x8e87xd][_0xcdfb[20]](_0xcdfb[31], function () {
+      window[_0xcdfb[34]](
+        _0x8e87x9[_0xcdfb[13]][_0x8e87xd][_0xcdfb[15]][_0xcdfb[32]],
+        _0xcdfb[33]
+      );
     });
-    list.appendChild(imageNodes[i]);
+    list[_0xcdfb[35]](_0x8e87xc[_0x8e87xd]);
   }
 }
